22dd343a2ec4d7315ef9649217dc8e6fa6097156
Edgar Ovidio Villareal Trevino <risear@Edgars-MacBook-Pro.local>
2020-01-22 19:07:15 +0000

    Updated movement of player with correct controls and behaviour.

--- Game.js
+++ Game.js
@@ -17,6 +17,9 @@ var GameObject = /** @class */ (function () {
     GameObject.prototype.getMesh = function () {
         return this._mesh;
     };
+    GameObject.prototype.changeFriction = function (frictionVal) {
+        this._mesh.physicsImpostor.friction = frictionVal;
+    };
     return GameObject;
 }());
 var Ground = /** @class */ (function (_super) {
@@ -24,7 +27,7 @@ var Ground = /** @class */ (function (_super) {
     function Ground(name, width, height, scene) {
         var _this = _super.call(this) || this;
         _this._mesh = BABYLON.Mesh.CreateGround("Ground", width, height, 2, scene);
-        _this._mesh.physicsImpostor = new BABYLON.PhysicsImpostor(_this._mesh, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0 }, scene);
+        _this._mesh.physicsImpostor = new BABYLON.PhysicsImpostor(_this._mesh, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0.0, friction: 1.0, restitution: 0.0 }, scene);
         return _this;
     }
     return Ground;
@@ -33,17 +36,45 @@ var Player = /** @class */ (function (_super) {
     __extends(Player, _super);
     function Player(name, size, scene) {
         var _this = _super.call(this) || this;
-        _this.velocityFactor = 25;
+        _this.movementOptions = [false, false, false, false];
+        _this.movementSpeed = 0.05;
         _this._mesh = BABYLON.Mesh.CreateBox(name, size, scene, true);
+        var myMaterial = new BABYLON.StandardMaterial("Purple", scene);
+        myMaterial.diffuseColor = new BABYLON.Color3(1, 0, 1);
+        _this._mesh.material = myMaterial;
         _this._mesh.position.y = 1;
-        _this._mesh.physicsImpostor = new BABYLON.PhysicsImpostor(_this._mesh, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 1, restitution: 1 }, scene);
+        _this._mesh.physicsImpostor = new BABYLON.PhysicsImpostor(_this._mesh, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 1.0, friction: 0.1, restitution: 1.0 }, scene);
         return _this;
     }
+    Player.prototype.changeMovement = function (move, value) {
+        this.movementOptions[move] = value;
+    };
+    Player.prototype.applyMovement = function () {
+        var movement = BABYLON.Vector3.Zero();
+        if (this.movementOptions[0]) {
+            movement.x -= this.movementSpeed;
+        }
+        if (this.movementOptions[1]) {
+            movement.x += this.movementSpeed;
+        }
+        if (this.movementOptions[2]) {
+            movement.z += this.movementSpeed;
+        }
+        if (this.movementOptions[3]) {
+            movement.z -= this.movementSpeed;
+        }
+        this._mesh.moveWithCollisions(movement);
+    };
     Player.prototype.movePlayerX = function (distance) {
-        this._mesh.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(distance * this.velocityFactor, 0, 0));
+        //this._mesh.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(distance * this.velocityFactor, 0, 0));
+        //this._mesh.translate(BABYLON.Vector3.Right(), distance, BABYLON.Space.WORLD);
+        if (this._mesh.physicsImpostor.friction != 0) {
+            this.changeFriction(0.0);
+            this._mesh.physicsImpostor.applyImpulse(new BABYLON.Vector3(distance, 0, 0), this._mesh.getAbsolutePosition());
+        }
     };
     Player.prototype.movePlayerY = function (distance) {
-        if (this._mesh.position.y <= 2) {
+        if (this._mesh.position.y <= 1.1) {
             this._mesh.physicsImpostor.applyImpulse(new BABYLON.Vector3(0, distance, 0), this._mesh.getAbsolutePosition());
         }
     };
@@ -60,37 +91,57 @@ var Game = /** @class */ (function () {
         this._scene = new BABYLON.Scene(this._engine);
         this._scene.enablePhysics(new BABYLON.Vector3(0, -9.81, 0));
         var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), this._scene);
-        //Add event listener for movement of the box
-        document.addEventListener("keydown", function (e) { return _this.handleMoveEvent(e); });
-        // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
-        //this._camera = new BABYLON.FreeCamera('FollowCamera', new BABYLON.Vector3(0, 0,-10), this._scene);
-        this._camera = new BABYLON.FollowCamera("FollowCamera", new BABYLON.Vector3(0, 5, -15), this._scene);
-        this._ground = new Ground("Ground", 25, 6, this._scene);
+        //Add key event listener for movement of the box
+        document.addEventListener("keydown", function (e) { return _this.handlePressEvent(e); });
+        document.addEventListener("keyup", function (e) { return _this.handleReleaseEvent(e); });
+        // Create a FreeCamera, and set its position to (x:0, y:10, z:-15).
+        this._camera = new BABYLON.FreeCamera('FollowCamera', new BABYLON.Vector3(0, 10, -15), this._scene);
+        this._ground = new Ground("Ground", 30, 30, this._scene);
         this._player = new Player("Player", 2.0, this._scene);
-        this._camera.lockedTarget = this._player.getMesh();
-        this._camera.radius = -10;
-        this._camera.heightOffset = 0;
-        this._camera.noRotationConstraint = true;
         this._camera.setTarget(BABYLON.Vector3.Zero());
         this._scene.activeCamera = this._camera;
     };
-    Game.prototype.handleMoveEvent = function (e) {
+    // Update movement array in Player to determine its movement
+    Game.prototype.handlePressEvent = function (e) {
         if (e) {
-            if (e.code === "ArrowLeft" || e.key === 'a') {
-                this._player.movePlayerX(-0.1);
+            if (e.code === "ArrowLeft") {
+                this._player.changeMovement(0, true);
             }
-            if (e.code === "ArrowRight" || e.key === 'd') {
-                this._player.movePlayerX(0.1);
+            if (e.code === "ArrowRight") {
+                this._player.changeMovement(1, true);
             }
-            if (e.code === "ArrowUp" || e.key === ' ') {
+            if (e.code === "ArrowUp") {
+                this._player.changeMovement(2, true);
+            }
+            if (e.code === "ArrowDown") {
+                this._player.changeMovement(3, true);
+            }
+            if (e.key === ' ') {
                 this._player.movePlayerY(5);
             }
+        }
+    };
+    // Update movement array in Player to determine its movement
+    Game.prototype.handleReleaseEvent = function (e) {
+        if (e.code === "ArrowLeft") {
+            this._player.changeMovement(0, false);
+        }
+        if (e.code === "ArrowRight") {
+            this._player.changeMovement(1, false);
+        }
+        if (e.code === "ArrowUp") {
+            this._player.changeMovement(2, false);
+        }
+        if (e.code === "ArrowDown") {
+            this._player.changeMovement(3, false);
         }
     };
     Game.prototype.runRenderLoop = function () {
         var _this = this;
         // Run the render loop.
         this._engine.runRenderLoop(function () {
+            // Apply movement depending on arrows pressed
+            _this._player.applyMovement();
             _this._scene.render();
         });
         // The canvas/window resize event handler.
--- Game.ts
+++ Game.ts
@@ -14,28 +14,55 @@ class Ground extends GameObject {
         super();
         this._mesh = BABYLON.Mesh.CreateGround("Ground", width, height, 2, scene);
         this._mesh.physicsImpostor = new BABYLON.PhysicsImpostor(this._mesh, BABYLON.PhysicsImpostor.BoxImpostor,
-            { mass: 0, restitution: 0 }, scene);
+            { mass: 0.0, friction: 1.0, restitution: 0.0 }, scene);
     }
 }
 
 class Player extends GameObject {
-    private velocityFactor: number;
+    private movementSpeed: number;
+    private movementOptions: boolean[];
 
     constructor(name: string, size: number, scene: BABYLON.Scene) {
         super();
-        this.velocityFactor = 25;
+        this.movementOptions = [false, false, false, false];
+        this.movementSpeed = 0.05;
         this._mesh = BABYLON.Mesh.CreateBox(name, size, scene, true);
+
+        let myMaterial = new BABYLON.StandardMaterial("Purple", scene);
+        myMaterial.diffuseColor = new BABYLON.Color3(1, 0, 1);
+        this._mesh.material = myMaterial;
+
         this._mesh.position.y = 1;
         this._mesh.physicsImpostor = new BABYLON.PhysicsImpostor(this._mesh, BABYLON.PhysicsImpostor.BoxImpostor,
-            { mass: 1, restitution: 1 }, scene);
+            { mass: 1.0, friction: 0.1, restitution: 1.0 }, scene);
     }
 
-    movePlayerX(distance: number): void {
-        this._mesh.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(distance * this.velocityFactor, 0, 0));
+    changeMovement(move: number, value: boolean): void {
+        this.movementOptions[move] = value;
+    }
+
+    applyMovement(): void {
+        var movement = BABYLON.Vector3.Zero();
+
+        if (this.movementOptions[0]) {
+            movement.x -= this.movementSpeed;
+        }
+        if (this.movementOptions[1]) {
+            movement.x += this.movementSpeed;
+        }
+
+        if (this.movementOptions[2]) {
+            movement.z += this.movementSpeed;
+        }
+        if(this.movementOptions[3]) {
+            movement.z -= this.movementSpeed;
+        }
+
+        this._mesh.moveWithCollisions(movement);
     }
 
     movePlayerY(distance: number): void {
-        if (this._mesh.position.y <= 2) {
+        if (this._mesh.position.y <= 1.1) {
             this._mesh.physicsImpostor.applyImpulse(new BABYLON.Vector3(0, distance, 0),
                 this._mesh.getAbsolutePosition());
         }
@@ -46,7 +73,7 @@ class Game {
     private _canvas: HTMLCanvasElement;
     private _engine: BABYLON.Engine;
     private _scene: BABYLON.Scene;
-    private _camera: BABYLON.FollowCamera;
+    private _camera: BABYLON.FreeCamera;
     private _player: Player;
     private _ground: Ground;
 
@@ -62,42 +89,64 @@ class Game {
 
         var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), this._scene);
 
-        //Add event listener for movement of the box
-        document.addEventListener("keydown", (e) => this.handleMoveEvent(e));
+        //Add key event listener for movement of the box
+        document.addEventListener("keydown", (e) => this.handlePressEvent(e));
+        document.addEventListener("keyup", (e) => this.handleReleaseEvent(e));
 
-        // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
-        //this._camera = new BABYLON.FreeCamera('FollowCamera', new BABYLON.Vector3(0, 0,-10), this._scene);
-        this._camera = new BABYLON.FollowCamera("FollowCamera", new BABYLON.Vector3(0, 5, -15), this._scene);
+        // Create a FreeCamera, and set its position to (x:0, y:10, z:-15).
+        this._camera = new BABYLON.FreeCamera('FollowCamera', new BABYLON.Vector3(0, 10,-15), this._scene);
 
-        this._ground = new Ground("Ground", 25, 6, this._scene);
+        this._ground = new Ground("Ground", 30, 30, this._scene);
         this._player = new Player("Player", 2.0, this._scene);
 
-        this._camera.lockedTarget = this._player.getMesh();
-        this._camera.radius = -10;
-        this._camera.heightOffset = 0;
-        this._camera.noRotationConstraint = true;
         this._camera.setTarget(BABYLON.Vector3.Zero());
 
         this._scene.activeCamera = this._camera;
     }
 
-    handleMoveEvent(e): void {
+    // Update movement array in Player to determine its movement
+    handlePressEvent(e): void {
         if (e) {
-            if (e.code === "ArrowLeft" || e.key === 'a') {
-                this._player.movePlayerX(-0.1);
+            if (e.code === "ArrowLeft") {
+                this._player.changeMovement(0, true);
             }
-            if (e.code === "ArrowRight" || e.key === 'd') {
-                this._player.movePlayerX(0.1);
+            if (e.code === "ArrowRight") {
+                this._player.changeMovement(1, true);
             }
-            if (e.code === "ArrowUp" || e.key === ' ') {
+            if (e.code === "ArrowUp") {
+                this._player.changeMovement(2, true);
+            }
+            if (e.code === "ArrowDown") {
+                this._player.changeMovement(3, true);
+            }
+
+            if (e.key === ' ') {
                 this._player.movePlayerY(5);
             }
+        }
+    }
+
+    // Update movement array in Player to determine its movement
+    handleReleaseEvent(e): void {
+        if (e.code === "ArrowLeft") {
+            this._player.changeMovement(0, false);
+        }
+        if (e.code === "ArrowRight") {
+            this._player.changeMovement(1, false);
+        }
+        if (e.code === "ArrowUp") {
+            this._player.changeMovement(2, false);
+        }
+        if (e.code === "ArrowDown") {
+            this._player.changeMovement(3, false);
         }
     }
 
     runRenderLoop(): void {
         // Run the render loop.
         this._engine.runRenderLoop(() => {
+            // Apply movement depending on arrows pressed
+            this._player.applyMovement();
             this._scene.render();
         });
 
